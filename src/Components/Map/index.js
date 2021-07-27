c894ea4aee8cf87d691b2356963f1cd8f369e3ba
PaulLeqx <lequeux.paul2@laposte.net>
2021-07-27 12:17:55 +0200

    getDeaprtementInfo on Hover

--- src/Components/Map/index.js
+++ src/Components/Map/index.js
@@ -1,14 +1,39 @@
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import franceDepartments from "@svg-maps/france.departments";
 import {SVGMap} from "react-svg-map";
 
 const Map = () => {
+  const [lastDepartementName, setLastDepartementName] = useState(null);
+  const [departementName, setDepartementName] = useState(null);
+  const [departementData, setDepartementData] = useState(null);
   const handleOver = (e) => {
     const departement = e.target;
-    const departementName = departement.getAttribute('name');
-    console.log(departementName);
-
+    setDepartementName(departement.getAttribute('name'));
   }
+
+  useEffect(() => {
+    if(departementName && !departementData || departementName !== lastDepartementName) {
+      const fetchData = async () => {
+        await axios.get(`https://coronavirusapi-france.now.sh/AllDataByDepartement?Departement=${departementName}`)
+        .then((response) => {
+          let departementAllData = response.data.allDataByDepartement;
+          let departementLastData = departementAllData[departementAllData.length-1];
+          setDepartementData(departementLastData);
+          setLastDepartementName(departementName);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+      }
+      fetchData();
+
+    }
+
+    if(departementData !== null && departementName === departementData.nom) {
+      console.log(departementData);
+    }
+  }, [departementName, departementData]);
 
   return (
     <div>
